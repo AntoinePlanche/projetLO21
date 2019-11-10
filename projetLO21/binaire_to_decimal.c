9760b6d359b7b594cf1422864b318129fdea2740
Grégori Mignerot <gmnrt@laposte.net>
2019-11-10 16:18:30 +0100

    Opti mineure

--- projetLO21/binaire_to_decimal.c
+++ projetLO21/binaire_to_decimal.c
@@ -8,7 +8,7 @@ int binaire_to_decimal(Individu l){
     Element* p = l;
     int result = 0, i = 0;
     while (p != NULL){
-        result += p->valeur * (1 << i); //puissance(2, i);
+        result += p->valeur << i; //puissance(2, i);
         p = p->suivant;
         i++;
     }
